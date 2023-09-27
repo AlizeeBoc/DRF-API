965a94b1c28aeef9b785969e51f4fb47989541ce
Alizee <bocianalizee@gmail.com>
2023-09-27 10:45:32 +0200

    problems with staff are ok now : acces to all crud

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
-    #path('auth/', obtain_auth_token),
-     path('auth/', admin.site.urls), # create an endpoint to genereate the tokens
+    path('auth/', obtain_auth_token),
+    # path('auth/', admin.site.urls), # create an endpoint to genereate the tokens
     path('', views.api_home) #localhost/api/
 ]
