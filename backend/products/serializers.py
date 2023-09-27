37c60dc3950d123d9187acb221280bcc9c77d26e
Alizee <bocianalizee@gmail.com>
2023-09-27 12:45:13 +0200

    urls ajoutés dans le serializer pour editer un produit ou consulter ses détails

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    #ne fonctionne que sur ModelSerializer :
+    url = serializers.HyperlinkedIdentityField(
+         view_name='product-detail',
+         lookup_field='pk'
+         )
     class Meta:
         model = Product
         fields = [
+            'url',
+            'edit_url',
             'pk',
             'title', 
             'content',
@@ -15,6 +23,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount',
         ]
 
+    def get_edit_url(self, obj):
+         #return f"/api/v2/products/{obj.pk}/"
+         request = self.context.get('request')
+         if request is None:
+              return None
+         return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+    #Si create product, les url seront automatiquement générées
+    
     def get_my_discount(self, obj):
             if not hasattr(obj, 'id'):
                  return None
