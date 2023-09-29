fb70bc13d735b7ee77afbcc049a25c89400c8e3e
Alizee <bocianalizee@gmail.com>
2023-09-29 13:51:26 +0200

    basic pagination

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -133,7 +133,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
-    ]
+    ],
+    "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE":10
 }
 
 # write on the view itself for specific setups
