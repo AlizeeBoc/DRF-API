32c8bb0e3a4f08623062945010cc1d11337a3477
Alizee <bocianalizee@gmail.com>
2023-09-29 13:23:04 +0200

    nested product datas

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,37 +1,48 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
+
+from api.serializers import UserPublicSerializer
 from .models import Product
 from .validators import validate_title
 
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+    view_name="product-detail", 
+    lookup_field="pk",
+    read_only=True
+    )
+    title = serializers.CharField(read_only=True)
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source="user", read_only=True)
+    related_products= ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
-    #ne fonctionne que sur ModelSerializer :
+    # ne fonctionne que sur ModelSerializer :
     url = serializers.HyperlinkedIdentityField(
-         view_name='product-detail',
-         lookup_field='pk'
+        view_name="product-detail", lookup_field="pk"
     )
     title = serializers.CharField(validators=[validate_title])
 
-    #name = serializers.CharField(source='title', read_only=True)
-    #email = serializers.EmailField(write_only=True)
     class Meta:
         model = Product
         fields = [
-            'user', # a commenter en prod;)
-            'url',
-            'edit_url',
-            #'email',
-            'pk',
-            'title',
-            #'name', 
-            'content',
-            'price',
-            'sale_price',
-            'my_discount',
+            "owner",  # user_id, a commenter en prod;)
+          #  "email",
+            "url",
+            "edit_url",
+            "pk",
+            "title",
+            "content",
+            "price",
+            "sale_price",
+            "my_discount",
+            "my_user_data",
+            'related_products',
         ]
 
-    #def validate_title(self, value):
+    # def validate_title(self, value):
     #    # request = self.context.get('request')
     #    # user = request.user
     #     qs = Product.objects.filter(title__iexact=value) #i pour case insensitive
@@ -39,30 +50,31 @@ class ProductSerializer(serializers.ModelSerializer):
     #          raise serializers.ValidationError(f"{value} is already a product name.")
     #     return value
 
-    #def create(self, validated_data):
+    # def create(self, validated_data):
     #    email = validated_data.pop('email')
     #    obj = super().create(validated_data)
     #    print(email, obj)
     #    return obj
-    
-    #def update(self, instance, validated_data):
+
+    # def update(self, instance, validated_data):
     #     email = validated_data.pop('email')
     #     return super().update(instance, validated_data)
 
+    def get_my_user_data(self, obj):
+        return {"username": obj.user.username}
+
     def get_edit_url(self, obj):
-         #return f"/api/v2/products/{obj.pk}/"
-         request = self.context.get('request')
-         if request is None:
-              return None
-         return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
-    #Si create product, les url seront automatiquement générées
+        # return f"/api/v2/products/{obj.pk}/"
+        request = self.context.get("request")
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request)
+
+    # Si create product, les url seront automatiquement générées
 
     def get_my_discount(self, obj):
-            if not hasattr(obj, 'id'):
-                 return None
-            if not isinstance(obj, Product):
-                 return None
-            return obj.get_discount()
-
-
-
+        if not hasattr(obj, "id"):
+            return None
+        if not isinstance(obj, Product):
+            return None
+        return obj.get_discount()
---
+++ py_client/basic.py
@@ -0,0 +1,13 @@
+import requests
+
+endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
+
+get_response = requests.post(endpoint, json={"title": "Abc123", "content": "Hello world", "price": "abc134"}) # HTTP Request
+# print(get_response.headers)
+# print(get_response.status_code)
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Notation ~ Python Dict
+print(get_response.json())
+# print(get_response.status_code)
